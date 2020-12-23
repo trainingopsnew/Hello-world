02998d82e831bb02bfbfbf349fa8feb03c686a36
trainer <trainer@gmail.com>
2020-12-23 20:20:38 +0530

    ds

--- Hello-world/Program.cs
+++ Hello-world/Program.cs
@@ -6,7 +6,7 @@ namespace Hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World one two m!");
+            Console.WriteLine("Hello World one two rm!");
 
             Console.WriteLine("My first app!");
         }
